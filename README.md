5c2e9e13023b497ca6d06d43eafb89e51edc9347
Roba <roba.vrstu@gmail.com>
2021-05-15 14:58:13 +0900

    デプロイするためのバッシュ作成と情報更新

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 # VRSTU
 
-# ビルド
+Django + Angular で作られた個人サイトである．
+
+## フロントエンドの開発サーバーの起動
 
 ```
-cd ui
-ng build --prod
+./run.sh ui
+```
+
+## バックエンドの開発サーバーの起動
+
+```
+./run.sh
+```
+
+## フロンドエンドのビルド
+
+```
+./run.sh build
+```
+
+## デプロイ
+
+```
+./run.sh deploy
 ```
--- run.sh
+++ run.sh
@@ -1,2 +1,23 @@
-cd ui
-ng serve --host 0.0.0.0 --open
+#!/bin/bash
+
+MODE=$1
+
+if [ "$MODE" = "build" ]; then
+  # ビルド
+  cd ui
+  npm run-script build -- --prod
+elif  "$MODE" = "deploy" ]; then
+  # デプロイ
+  cd ui
+  npm run-script build -- --prod
+  cd ..
+  python manage.py makemigrations blog
+  python manage.py migrate
+  python manage.py collectstatic
+elif [ "$MODE" = "ui" ]; then
+  # フロントエンドの開発サーバー起動
+  cd ui
+  npm run-script start -- --host 0.0.0.0
+else
+  python manage.py runserver 0.0.0.0:8000
+fi
